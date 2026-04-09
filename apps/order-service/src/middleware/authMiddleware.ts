8d9940bb7fc2ce2e0ad7dc8dc9f6ba7372c99834
azpradipta <aryazakypradipta@gmail.com>
2026-04-09 21:08:44 +0800

    feat : add Feastify API for order-service and role based auth middleware

--- apps/order-service/src/middleware/authMiddleware.ts
+++ apps/order-service/src/middleware/authMiddleware.ts
@@ -1,5 +1,6 @@
 import { FastifyReply, FastifyRequest } from "fastify";
 import { clerkPlugin, getAuth } from '@clerk/fastify'
+import type { CustomJwtSessionClaims } from "@repo/types";
 
 declare module "fastify"{
     interface FastifyRequest {
@@ -10,8 +11,23 @@ declare module "fastify"{
 export const shouldBeUser = async (request:FastifyRequest, reply:FastifyReply) => {
     const { userId } = getAuth(request)
     if(!userId) {
-        return reply.send({ message: "You are not logged in!" });
+        return reply.status(401).send({ message: "You are not logged in!" });
     }
 
     request.userId = userId;
+};
+
+export const shouldBeAdmin = async (request:FastifyRequest, reply:FastifyReply) => {
+    const auth = getAuth(request)
+    if(!auth.userId) {
+        return reply.status(401).send({ message: "You are not logged in!" });
+    }
+
+    const claims = auth.sessionClaims as CustomJwtSessionClaims;
+
+    if(claims.metadata?.role !== "admin") {
+        return reply.status(403).send({ message: "Unauthorized" });
+    } 
+
+    request.userId = auth.userId;
 };
